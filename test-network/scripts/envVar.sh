b3946d005d8a9f4418fddeee2960fe8e320896aa
tjddnr7760 <tjddnr7760@naver.com>
2022-11-16 23:04:18 +0900

    feat : 체인 코드 배포 함수 일부 구현

--- test-network/scripts/envVar.sh
+++ test-network/scripts/envVar.sh
@@ -48,7 +48,6 @@ setGlobals() {
     export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
     export NUM="ch1"
-    echo ${PEER0_ORG1_CA} "출력문"
   elif [ $USING_ORG == 'org1' ] && [ $MYPEER -eq 1 ]; then
     export CORE_PEER_LOCALMSPID="Org1MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER1_ORG1_CA
